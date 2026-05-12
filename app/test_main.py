74dca3205dd6fb6302ab9f43b4f4c5bce107bed1
ToJaVadym <vadymivaneiko2@gmail.com>
2026-05-12 20:11:06 +0200

    Fixed tests and main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,22 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_14_cat_or_dog_years_should_convert_into_0_human_age():
-    assert get_human_age(0, 0) == [0, 0]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (0, 0, [0, 0]),
 
+        (16, 20, [1, 1]),
 
-def test_should_return_1_when_cat_and_dog_age_is_greater_than_15_and_lower_than_24():
-    assert get_human_age(16, 20) == [1, 1]
+        (27, 28, [2, 2]),
 
+        (28, 29, [3, 2]),
 
-def test_should_return_2_when_cat_age_is_greater_than_24_and_lower_than_28():
-    assert get_human_age(27, 28) == [2, 2]
-
-
-def test_should_return_3_when_cat_age_is_greater_or_equal_to_28():
-    assert get_human_age(28, 25) == [3, 2]
-
-
-def test_should_return_2_when_dog_age_is_greater_than_24_and_lower_than_29():
-    assert get_human_age(27, 25) == [2, 2]
-
-
-def test_should_return_3_when_dog_age_is_greater_or_equal_to_29():
-    assert get_human_age(30, 29) == [3, 3]
+        (32, 34, [4, 3]),
+    ]
+)
+def test_get_human_age_conversion(cat_age: int,
+                                  dog_age: int,
+                                  expected: list[int]) -> None:
+    assert get_human_age(cat_age, dog_age) == expected

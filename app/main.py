93a588882f274c0f3fdc789783bec082d7ad40ce
ToJaVadym <vadymivaneiko2@gmail.com>
2026-05-12 20:25:01 +0200

    fixed main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 def get_human_age(cat_age: int, dog_age: int) -> list:
     if cat_age <= 14:
         cat_human = 0
-    elif cat_age <= 24:
+    elif cat_age <= 23:
         cat_human = 1
     else:
         cat_human = 2 + (cat_age - 24) // 4
     if dog_age <= 14:
         dog_human = 0
-    elif dog_age <= 24:
+    elif dog_age <= 23:
         dog_human = 1
     else:
-        dog_human = 2 + (dog_age - 25) // 5
+        dog_human = 2 + (dog_age - 24) // 5
     return [cat_human, dog_human]

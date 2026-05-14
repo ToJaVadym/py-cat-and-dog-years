b793287cdc2a7cfa905c5228957e013b77ed2655
ToJaVadym <vadymivaneiko2@gmail.com>
2026-05-14 21:45:48 +0200

    Changed tests

--- app/main.py
+++ app/main.py
@@ -1,14 +1,22 @@
 def get_human_age(cat_age: int, dog_age: int) -> list:
+    if not isinstance(cat_age, int) or not isinstance(dog_age, int):
+        raise TypeError
+
+    if not (0 <= cat_age <= 100) or not (0 <= dog_age <= 100):
+        raise ValueError
+
     if cat_age <= 14:
         cat_human = 0
     elif cat_age <= 23:
         cat_human = 1
     else:
         cat_human = 2 + (cat_age - 24) // 4
+
     if dog_age <= 14:
         dog_human = 0
     elif dog_age <= 23:
         dog_human = 1
     else:
         dog_human = 2 + (dog_age - 24) // 5
+
     return [cat_human, dog_human]
--- app/test_main.py
+++ app/test_main.py
@@ -6,23 +6,25 @@ from app.main import get_human_age
     "cat_age, dog_age, expected",
     [
         (0, 0, [0, 0]),
-
         (14, 14, [0, 0]),
-
         (15, 15, [1, 1]),
-
         (23, 23, [1, 1]),
-
         (24, 24, [2, 2]),
-
         (27, 27, [2, 2]),
-
         (28, 28, [3, 2]),
-
-        (100, 100, [21, 17])
+        (29, 29, [3, 3]),
+        (100, 100, [21, 17]),
+        (200, 200, ValueError),
+        (-1, 10, ValueError),
+        ("10", 10, TypeError),
+        (None, 10, TypeError),
     ]
 )
 def test_get_human_age_conversion(cat_age: int,
                                   dog_age: int,
                                   expected: list[int]) -> None:
-    assert get_human_age(cat_age, dog_age) == expected
+    if isinstance(expected, type):
+        with pytest.raises(expected):
+            get_human_age(cat_age, dog_age)
+    else:
+        assert get_human_age(cat_age, dog_age) == expected
